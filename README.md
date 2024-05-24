f11a7ce0f3af75d0f4aa5ae5bd23cea257550767
Kishan Bapodra <kishanbapodra2002@gmail.com>
2024-05-24 17:20:40 +0400

    Add README

---
+++ README.md
@@ -0,0 +1,24 @@
+# TranslateThis 
+
+Backend server for translateThis using Anthropic Claude API.
+
+### Install the dependancies:
+```
+npm insall
+```
+
+### ENV Variables:
+
+Add the following env variables: 
+```
+SECRET_KEY=
+MONGODB_URI=
+PORT=3333
+DOMAIN=http://localhost:5173
+ANTHROPIC_API_KEY=
+```
+
+### Run the server
+```
+node app.js
+```
--- app.js
+++ app.js
@@ -10,14 +10,15 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3333;
+const DOMAIN = process.env.DOMAIN || "http://localhost:5173";
 
 app.use(express.json());
 app.use(cookieParser());
 
 // CORS Headers
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.DOMAIN);
+  res.header("Access-Control-Allow-Origin", DOMAIN);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
